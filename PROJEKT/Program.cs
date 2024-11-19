10f586c950120ba3afb5c61367b748eab7379362
TekPra <antoni.pralat@uczen.zsk.poznan.pl>
2024-11-19 12:36:57 +0100

    Fix conflicts

--- PROJEKT/Program.cs
+++ PROJEKT/Program.cs
@@ -19,71 +19,73 @@ namespace PROJEKT
                 Console.WriteLine("Could not register user, try again");
             }
         }
-        class User
+        }
+    }
+    class User
+    {
+        public bool IsRegistered = false;
+        private string Email;
+        private string Password;
+        protected byte[] Salt;
+        public bool HasSpecialChars(string yourString)
         {
-            public bool IsRegistered=false;
-            private string Email;
-            private string Password;
-            protected byte[] PasswordHash;
-            public bool HasSpecialChars(string yourString)
+            return yourString.Any(ch => !char.IsLetterOrDigit(ch));
+        }
+        public bool HasUpperCase(string yourString)
+        {
+            return yourString.Any(ch => char.IsUpper(ch));
+        }
+        public bool HasNumber(string yourString)
+        {
+            return yourString.Any(ch => char.IsNumber(ch));
+        }
+        public bool IsEmailCorrect(string email)
+        {
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email);
+            if (match.Success)
             {
-                return yourString.Any(ch => !char.IsLetterOrDigit(ch));
+                return true;
             }
-            public bool HasUpperCase(string yourString) {
-                return yourString.Any(ch=> char.IsUpper(ch));
-            }
-            public bool HasNumber(string yourString)
+            else
             {
-                return yourString.Any(ch=>char.IsNumber(ch));
+                return false;
             }
-            public bool IsEmailCorrect(string email)
+        }
+        public void Print()
+        {
+            Console.WriteLine($" Email:{this.Email}\n Password:{this.Password}\n Hash:{Convert.ToBase64String(this.Salt)}");
+        }
+        public void Register()
+        {
+            string email = Console.ReadLine();
+            string password = Console.ReadLine();
+            string password2 = Console.ReadLine();
+            if (email != null && password != null && password == password2 && IsEmailCorrect(email))
             {
-                Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
-                Match match = regex.Match(email);
-                if (match.Success) {
-                    return true;
+                if (password.Length > 7 && HasSpecialChars(password) && HasUpperCase(password))
+                {
+                    byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+                    string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                        password: password!,
+                        salt: salt,
+                        prf: KeyDerivationPrf.HMACSHA256,
+                        iterationCount: 100000,
+                        numBytesRequested: 256 / 8));
+                    this.Email = email;
+                    this.Password = hashed;
+                    this.Salt = salt;
+                    this.IsRegistered = true;
+                    Console.WriteLine("Zarejestrowano pomyślnie");
                 }
                 else
                 {
-                    return false;
+                    Console.WriteLine("Password must contain atleast 8 characters, 1 special character ,1 uppercase character and 1 number");
                 }
             }
-            public void Print()
+            else
             {
-                Console.WriteLine($" Email:{this.Email}\n Password:{this.Password}\n Hash:{Convert.ToBase64String(this.PasswordHash)}");
-            }
-            public void Register()
-            {
-                string email = Console.ReadLine();
-                string password = Console.ReadLine();
-                string password2 = Console.ReadLine();
-                if (email != null && password != null && password == password2 && IsEmailCorrect(email))
-                {
-                    if (password.Length > 7 && HasSpecialChars(password) && HasUpperCase(password))
-                    {
-                        byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
-                        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                            password: password!,
-                            salt: salt,
-                            prf: KeyDerivationPrf.HMACSHA256,
-                            iterationCount: 100000,
-                            numBytesRequested: 256 / 8));
-                        this.Email = email;
-                        this.Password = hashed;
-                        this.PasswordHash = salt;
-                        this.IsRegistered = true;
-                        Console.WriteLine("Zarejestrowano pomyślnie");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Password must contain atleast 8 characters, 1 special character ,1 uppercase character and 1 number" ) ;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Niepoprawne dane");
-                }
+                Console.WriteLine("Niepoprawne dane");
             }
         }
     }
-}
--- PROJEKT/obj/Debug/net8.0/PROJEKT.AssemblyInfo.cs
+++ PROJEKT/obj/Debug/net8.0/PROJEKT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROJEKT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81b5f34e724b5f83d554638a238cf6c83970d25b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0067f9d452348a088065463703891167cd24ed34")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROJEKT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROJEKT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
