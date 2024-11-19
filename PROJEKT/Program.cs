6a4282fe813768a5606473649355f61ea1d99f06
KacperCieslinski <kacper.cieslinski@uczen.zsk.poznan.pl>
2024-11-19 11:50:27 +0100

    commit added new classes

--- PROJEKT/Program.cs
+++ PROJEKT/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace PROJEKT
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Diagnostics;
+using System.Drawing;
+using System;
+using System.Text;
+using System.Dynamic;
+
+namespace PROJEKT
 {
     internal class Program
     {
@@ -6,23 +13,33 @@
         {
             Console.WriteLine("Hello, World!");
         }
-        public class Car(double enginecapacity, string suspension, string color, int horsepower, int vmax,
-            int year, string fueltype, string brand, double price, double fuelconsumptioncity,
-            double fuelconsumptiontrip, int range)
-        {
-            double EngineCapacity = enginecapacity;
-            string Suspension = suspension;
-            string Color = color;
-            int HorsePower = horsepower;
-            int Vmax = vmax;
-            int Year = year;
-            string Fuel = fueltype;
-            string Brand = brand;
-            double Price = price;
-            double FuelConsumptionCity = fuelconsumptioncity;
-            double FuelConsumptionTrip = fuelconsumptiontrip;
-            int Range = range;
 
-        }
+        
+
+    }
+    public class CarInfo(double enginecapacity, string suspension, string color, int horsepower, int vmax,
+        int year, string fueltype, double price, double fuelconsumptioncity,
+        double fuelconsumptiontrip, int range)
+    {
+        double EngineCapacity = enginecapacity;
+        string Suspension = suspension;
+        string Color = color;
+        int HorsePower = horsepower;
+        int Vmax = vmax;
+        int Year = year;
+        string Fuel = fueltype;
+        double Price = price;
+        double FuelConsumptionCity = fuelconsumptioncity;
+        double FuelConsumptionTrip = fuelconsumptiontrip;
+        int Range = range;
+
+    }
+    public class Car(CarInfo cinfo)
+    {
+        CarInfo CarInfo = cinfo;
+    }
+    public class BMW(CarInfo cinfo, string model) : Car(cinfo)
+    {
+        string Model = model;
     }
 }
